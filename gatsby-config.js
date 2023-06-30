d16543efefeb7544ff3a22c9363e503fc5e2bbc3
김하영 <khy4018@gmail.com>
2023-06-30 17:12:17 +0900

    :wrench: Update image quality

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,6 +66,7 @@ module.exports = {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 630,
+              quality: 100
             },
           },
           {
