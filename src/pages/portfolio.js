e4aad0b7b620a13e5524c82344dc16a7675b1f22
김하영 <khy4018@gmail.com>
2024-06-12 13:23:57 +0900

    :white_check_mark: 포트폴리오 내용 수정 및 추가

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -10,7 +10,7 @@ const Portfolio = ({ data, location }) => {
       period: "2021.10 - 2024.05",
       projects: [
         {
-          name: "밀키스 프로모션 이벤트",
+          name: "밀키스 친밀리미터",
           period: "2024.04 ~ 2024.05",
           description: "롯데칠성음료 밀키스 프로모션 이벤트 페이지 프론트엔드를 설계하고 제작했습니다.",
           whatIDid: [
@@ -23,9 +23,9 @@ const Portfolio = ({ data, location }) => {
           techStack: "React, Redux, SCSS, Git"
         },
         {
-          name: "사내 인트라넷 구축",
+          name: "사내 인트라넷 STICK WORKS",
           period: "2022.10 ~ 2024.04",
-          description: "사내 인트라넷 서비스인 Stick Works의 프론트엔드를 개발했습니다. 기본적인 유지보수를 비롯해 비즈니스의 요구에 맞는 다양한 새로운 기능을 개발했습니다.",
+          description: "사내 인트라넷 서비스인 <a href='https://intranet.stickint.com' target='_blank'>STICK WORKS</a>의 프론트엔드를 개발했습니다.<br/>기본적인 유지보수를 비롯해 비즈니스의 요구에 맞는 다양한 새로운 기능을 개발했습니다.",
           whatIDid: [
             "React 기반 컴포넌트 계층 구조 디자인",
             "reactstrap, scss 기반 반응형 작업",
@@ -36,7 +36,7 @@ const Portfolio = ({ data, location }) => {
         {
           name: "한우자조금 사이트 리뉴얼",
           period: "2023.08 ~ 2023.10",
-          description: "한우자조금 홈페이지의 리뉴얼 구축 작업에서 UI를 개발했습니다.",
+          description: "<a href='https://www.hanwooboard.or.kr/' target='_blank'>한우자조금 홈페이지</a>의 리뉴얼 구축 작업에서 UI를 개발했습니다.",
           whatIDid: [
             "적응형 페이지로 Scss 기반 퍼블리싱",
             "chart.js 이용한 데이터 시각화",
@@ -78,7 +78,8 @@ const Portfolio = ({ data, location }) => {
       name: "Communication",
       content: [
         "직위 및 포지션에 관계없이 적극적으로 생각을 표현합니다.",
-        "커뮤니케이션은 적은 것보다는 많은게 좋다고 믿습니다."
+        "커뮤니케이션은 적은 것보다는 많은게 좋다고 믿습니다.",
+        "효과적인 회의 진행 및 의사결정 과정을 위해 다양한 협업 도구(JIRA, SLACK 등)를 활용할 줄 압니다.",
       ]
     },
     {
@@ -86,7 +87,8 @@ const Portfolio = ({ data, location }) => {
       content: [
         "검색엔진최적화(SEO) 경험이 있습니다.",
         "Google 애널리틱스 경험이 있습니다.",
-        "모바일 브라우저에서의 트러블 슈팅 경험이 많습니다."
+        "모바일 브라우저에서의 트러블 슈팅 경험이 많습니다.",
+        "API 통합 및 비동기 작업을 원활하게 처리할 수 있습니다."
       ]
     },
     {
@@ -94,13 +96,25 @@ const Portfolio = ({ data, location }) => {
       content: [
         "Javascript에 능숙하며 Typescript를 사용할 줄 압니다.",
         "CommonJS, ES Modules의 모듈 시스템에 대해서 이해하고 이에 따라 적절한 도구를 활용합니다.",
+        "ES6+의 최신 문법과 기능을 활용하여 효율적인 코드를 작성합니다."
       ]
     },
     {
       name: "React",
       content: [
         "React hooks를 적절하게 사용하고, 대부분의 컴포넌트를 함수로 만듭니다. hook을 이용해 공통 비즈니스 로직을 적절히 모듈화해 사용할 수 있습니다.",
-        "합리적인 방식으로 컴포넌트를 분리할 줄 압니다."
+        "합리적인 방식으로 컴포넌트를 분리할 줄 압니다.",
+        "Redux와 Context API를 사용한 상태 관리 경험이 있습니다.",
+        "Styled-components와 Emotion을 사용한 CSS-in-JS 스타일링 경험이 있습니다."
+      ]
+    },
+    {
+      name: "Vue",
+      content: [
+        "Vuex를 사용한 상태 관리에 능숙합니다.",
+        "컴포넌트 기반 아키텍처를 이해하고 효율적으로 활용할 수 있습니다.",
+        "Vue Router를 사용한 라우팅 설정 및 네비게이션 구현 경험이 있습니다.",
+        "Vue CLI를 활용한 프로젝트 셋업 및 관리 경험이 있습니다."
       ]
     }
   ]
@@ -131,8 +145,9 @@ const Portfolio = ({ data, location }) => {
             <h1>Hayeong Kim</h1>
             <p className={styles.bigParagraph}>
               안녕하세요. 3년차 웹 개발자 김하영입니다.<br/>
-              사람들에게 도움이 되는 일을 하고 싶어서 개발자가 되었습니다.<br/>
-              아름다운 인터페이스를 중요하게 생각합니다.
+              아름다운 인터페이스와 애니메이션을 구현하는 것을 좋아합니다.<br/>
+              사람들을 돕는 것에 큰 가치와 효용을 느끼며,<br/>항상 새로운 것을 배우고 성장하는 것에 가치를 둡니다.<br/>
+              UX와 DX를 모두 고려하는 설계를 하려 노력하고 있습니다.
             </p>
           </section>
           <main>
@@ -151,7 +166,7 @@ const Portfolio = ({ data, location }) => {
                         <h3>{project.name}</h3>
                         <span>{project.period}</span>
                         <h4>Description</h4>
-                        <p>{project.description}</p>
+                        <p dangerouslySetInnerHTML={{ __html: project.description }}></p>
                         <h4>What did I do</h4>
                         <ul>
                           {project.whatIDid.map((content) => (
@@ -171,7 +186,7 @@ const Portfolio = ({ data, location }) => {
               <div className={styles.other}>
                 <h3>Voca Vaca</h3>
                 <span>2023.02 ~ 2023.03</span>
-                <p>개인 프로젝트로 영어 단어를 외울 수 있는 영어 암기장을 개발했습니다.<br/>
+                <p>개인 프로젝트로 영어 단어를 외울 수 있는 영어 암기장 <a href="https://kirahaa.github.io/vocabulary-learning/" target="_blank">Voca Vaca</a>을 개발했습니다.<br/>
                   기획, 디자인, 개발까지 모두 혼자 진행했으며, React를 공부하는 용도로 만들었습니다.
                 </p>
               </div>
